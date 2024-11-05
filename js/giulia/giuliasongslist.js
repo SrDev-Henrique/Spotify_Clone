0d4c348758bf11d34e4d518a101644f1b37b049c
Henrique Albuquerque <halbuquerque2850@gmail.com>
2024-11-05 18:25:55 -0300

    Add Giulia´s Playlist page and songs

---
+++ js/giulia/giuliasongslist.js
@@ -0,0 +1,56 @@
+const allMusics = [
+    {
+        name: "Play",
+        artist: "Anny, Slow GM",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Garoto de Ouro",
+        artist: "Shooter-sz",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Idol",
+        artist: "Shiny",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Tsar",
+        artist: "kamaitachi",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Infame",
+        artist: "Supercombo, kamaitachi",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Do lado de cá",
+        artist: "TakaB, Anny",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "Prisão de vidro",
+        artist: "TakaB",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "Rei dos Pesadelos",
+        artist: "TakaB",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "Sozinho",
+        artist: "Tom Filho",
+        img: "music-9",
+        src: "music-9"
+    }
+]
